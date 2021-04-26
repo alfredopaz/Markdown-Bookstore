a9341d86e24904f73732e71357f3fd12dd5d75dd
Alfredo Paz Valderrama <Alfredo Paz>
2021-04-25 20:48:13 -0500

    Implementación del ejemplo del poema, se usa lectura de archivos asincrona y expresiones regulares en NodeJS

--- pub/test.js
+++ pub/test.js
@@ -1,2 +1,10 @@
-console.log('Hola Mundo!');
-
+function recitar(){
+  const url = 'http://localhost:3000/recitar'
+  fetch(url).then(
+    response => response.json()
+  ).then(
+    data => {
+      document.querySelector("#poema").innerHTML = data.text
+    }
+  )
+}
