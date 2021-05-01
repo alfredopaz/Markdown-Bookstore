1be3dc27ef41a1aa7c4509de0be8abdfe2bf6ad9
Alfredo Paz Valderrama <Alfredo Paz>
2021-04-30 19:44:01 -0500

    modificaciones para el servidor MarkDown

--- markDownServer.js
+++ markDownServer.js
@@ -1,26 +1,22 @@
 const fs = require('fs')
 const path = require('path')
 const express = require('express')
+const MarkdownIt = require('markdown-it'),
+    md = new MarkdownIt();
 const app = express()
 app.use(express.static('pub'))
 
 app.listen(3000, () => {
   console.log("Escuchando en: http://localhost:3000")
-});
+})
 
 app.get('/', (request, response) => {
   response.sendFile(path.resolve(__dirname, 'index.html'))
 })
 
-app.get('/recitar', (request, response) => {
-  fs.readFile(path.resolve(__dirname, 'priv/poema.txt'), 'utf8', 
-    (err, data) => {
-      if(err){
-        console.error(err)
-        response.status(500).json({ error: 'message' })
-        return
-      }
-      response.json({text: data.replace(/\n/g, '<br>') })
-
-     })
+app.post('/', (request, response) => {
+  let markDownText = request.body.text
+  let htmlText = md.render(markDownText)
+  response.setHeader('Content-Type', 'application/json');
+  response.end(JSON.stringify({text:htmlText})
 })
--- pub/test.js
+++ pub/test.js
@@ -1,10 +1,28 @@
-function recitar(){
-  const url = 'http://localhost:3000/recitar'
-  fetch(url).then(
+function recitar(markupText){
+  const markupText = document.querySelector('#markupText')
+  const url = 'http://localhost:3000/'
+  http = fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      text: markupText
+    })
+  })
+  http.then(
     response => response.json()
   ).then(
     data => {
-      document.querySelector("#poema").innerHTML = data.text
+      document.querySelector("#htmlCode").innerHTML = data.text
     }
   )
 }
+document.addEventListener('DOMContentLoaded', function(){
+  const text = document.querySelector('#markupText')
+  document.querySelector('#form1').onsubmit = () => {
+    recitar(text.value)
+    return false;
+  }
+
+})
